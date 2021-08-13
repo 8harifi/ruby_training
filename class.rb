8a4a9e220fa8135ff8e6bcda1f4e168c42c5266a
8harifi <8harifi@gmail.com>
2021-08-13 16:06:38 +0430

    some changes in class.rb

--- class.rb
+++ class.rb
@@ -1,20 +1,17 @@
 class Number
-    attr_accessor :number
+    attr_accessor :number, :devided_by_10, :devided_by_100
+    def initialize(number)
+        @number = number
+        @devided_by_10 = number/10
+        @devided_by_100 = number/100
+    end
     def devide_by(num)
         puts self.number/num
     end
-    def devided_by_10()
-        return self.number/10
-    end
-    def devided_by_100()
-        return self.number/100
-    end
 end
 
-number1 = Number.new()
-number1.number = 100
-res1 = number1.devided_by_10()
-res2 = number1.devided_by_100()
-puts res1
-puts res2
+number1 = Number.new(100)
+
+puts number1.devided_by_10
+puts number1.devided_by_100
 number1.devide_by(5)
